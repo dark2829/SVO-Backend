a464fef388f1548453aa483b10c1a24c86e8e7f0
EsMerari <eslimerari.sanluis@gmail.com>
2022-05-26 15:50:13 -0500

    eliminar tarjetas y direcciones

--- src/main/java/com/svo/svo/service/TdireccionService.java
+++ src/main/java/com/svo/svo/service/TdireccionService.java
@@ -1,4 +1,9 @@
 package com.svo.svo.service;
 
+import com.svo.svo.other.Utils.AppException;
+
 public interface TdireccionService {
+
+
+    void deleteDireccion(Long idDireccion, Long idPersona) throws AppException;
 }
